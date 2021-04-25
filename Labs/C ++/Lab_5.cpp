7c9e39176fa25ef7182e282f95d50fa55c873925
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2021-04-25 21:19:43 +0300

    Update Lab_5.cpp

--- Labs/C ++/Lab_5.cpp
+++ Labs/C ++/Lab_5.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <algorithm>
 #include <ctime>
 #include <cstdlib>
+#include <cmath>
 using namespace std;
 
 
@@ -198,7 +200,7 @@ int main()
 
 
 
-*/
+
 
 
 // Задача 5
@@ -208,13 +210,13 @@ int main()
 	setlocale(LC_ALL, "rus");
 	
 	int a,x,y;
-	int q,w,z;
+	int q,w,z,t;
 	int i = 0;
 	srand(time(NULL));
 	
 	cout << "Введите количество чисел в массиве" << endl;
 	cin >> a;
-	int Arr[a] = {};
+	int *Arr = new int[a];
 	
 	cout << "Введите максимум и минимум" << endl;
 	cin >> x >> y;
@@ -223,6 +225,8 @@ int main()
 	
 	cout << "Введите интервал" << endl;
 	cin >> q >> w;
+	z = 0;
+	t = 0;
 	
 	for(i = 0; i < a; i++) // Создание массива
 	{
@@ -230,30 +234,350 @@ int main()
 		cout << Arr[i] << " | ";
 	}
 	cout << endl << endl;
-	for(i = 0; i < a; i++)
+	
+	for(i = 0; i < a; i++) // Сжатие массива
 	{
 		if(Arr[i] >= q && Arr[i] <= w)
 		{
-			Arr[i] = 0;
+			z++;
+			for(t = i; t < a; t++)
+			{
+				Arr[t] = Arr[t + 1];
+				i--;
+			}
 		}
 	}
-	do
-	{
-		
-		if(Arr[i] == 0 && Arr[i + 1] != 0)
-		{
-			z = Arr[i];
-			Arr[i] = Arr[i + 1];
-			Arr[i + 1] = z;
-			i++;
-		}
-	}while(i != a);
 	
-	for(i = 0; i < a; i++) // Создание массива
+	  for (int i = 0; i < a; i++)
+    {
+        if (i > a - 1 - z)
+        {
+        	Arr[i] = 0;
+		}
+    }
+ 
+        for (int i = 0; i < a; i++)
+    {
+        cout << Arr[i] << " | ";
+    }
+    cout << endl;
+	
+
+
+	return 0;
+	
+}
+
+
+
+
+// Задача 6
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	int a,x,y;
+	int i,z,t;
+	
+	srand(time(0));
+	
+		
+	cout << "Введите количество чисел в массиве" << endl;
+	cin >> a;
+	float *Arr = new float[a];
+	
+	cout << "Введите максимум и минимум" << endl;
+	cin >> x >> y;
+	const int upperlim = x;
+	const int lowerlim = y;
+	
+	z = 0;
+	t = 0;
+	for(i = 0; i < a; i++)
 	{
+		Arr[i] = y + rand() % (y - x + 1);
 		cout << Arr[i] << " | ";
 	}
 	
+	
+	for(i = 0; i < a; i++)
+	{
+		if(Arr[i] < 0)
+		{
+			for(z = i + 1; z < a; z++)
+			{
+			t += abs(Arr[z]);
+			}
+		}
+		break;
+	}
+	cout << endl;
+	cout << "Сумма модулей массива находящихся после первого отрицательного элемента составляет " << t << endl;
+	
+	
+	return 0;
+}
+
+
+
+
+// Задача 7
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	int a,x,y;
+	int i,z,t;
+	
+	srand(time(0));
+	
+		
+	cout << "Введите количество чисел в массиве" << endl;
+	cin >> a;
+	float *Arr = new float[a];
+	
+	cout << "Введите максимум и минимум" << endl;
+	cin >> x >> y;
+	const int upperlim = x;
+	const int lowerlim = y;
+	
+	z = x + 1;
+	
+	for(i = 0; i < a; i++)
+	{
+		Arr[i] = y + rand() % (x - y + 1);
+		cout << Arr[i] << " | ";
+	}
+	
+	cout << endl << endl;
+	
+	for(i = 0; i < a; i++)
+	{
+		Arr[i] = abs(Arr[i]);
+	}
+	
+	for(i = 0; i < a; i++)
+	{
+		if(Arr[i] < z)
+		{
+			z = Arr[i];
+			t = i + 1;
+		}
+	}
+	cout << "Номер минимального значения массива по модулю равен " << t << endl;
+	
+	return 0;
+}
+
+
+
+
+// Задача 8 
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	int a,x,y;
+	int i,z,t;
+	
+	srand(time(0));
+	
+		
+	cout << "Введите количество чисел в массиве" << endl;
+	cin >> a;
+	int *Arr = new int[a];
+	
+	cout << "Введите максимум и минимум" << endl;
+	cin >> x >> y;
+	const int upperlim = x;
+	const int lowerlim = y;
+	
+	z = 0;
+	
+	for(i = 0; i < a; i++)
+	{
+		Arr[i] = y + rand() % (x - y + 1);
+		cout << Arr[i] << " | ";
+	}
+	
+	cout << endl << endl;
+	
+	
+	for(i = 0; i < a; i++)
+	{
+		for(abs(Arr[i]) > 0; Arr[i] != 0; abs(Arr[i]) / 10)
+		{
+			if(Arr[i] == 10)
+			{
+				z += 1;
+				i++;
+			}
+			if(Arr[i] == 0)
+			{
+				z += 0;
+			}
+			else
+			{
+				z += abs(Arr[i]) % 10;
+				Arr[i] /= 10;
+				cout << "z = " << z << endl;
+			}
+		}
+	}
+	
+	cout << "Сумма всех цифр целочисленного массива = " << z << endl;
+	
+	return 0;
+}
+
+
+
+
+
+
+// Задача 9
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	int x,y;
+	int i,z,t,c,d;
+	
+	srand(time(0));
+	
+	int a,b;
+		
+	cout << "Введите количество строк и столбцов в массиве" << endl;
+	cin >> a >> b;
+	
+	const int Row = a;
+	const int Col = b;
+	
+	
+	
+	cout << "Введите максимум и минимум" << endl;
+	cin >> x >> y;
+	const int upperlim = x;
+	const int lowerlim = y;
+	
+	
+	int Matrix[Row][Col] = {};
+	
+	for (c = 0; c < Row; c++)
+	{
+		for(d = 0; d < Col; d++)
+		{
+			Matrix[c][d] = rand() % (x - y + 1);
+			if(Matrix[c][d] < 10) // немного красивости из-за выравнивания вертикальной черты. Работает до 3х значных чисел
+			{
+				cout << Matrix[c][d] << "  | ";
+			}
+			else 
+			{
+				cout << Matrix[c][d] << " | ";
+			}
+		}
+		cout << endl;
+	}
+	
+	cout << endl << endl;
+	
+	for (c = 0; c < Row; c++)
+	{
+		for(d = 0; d < Col; d++)
+		{
+			Matrix[c][d] *= Matrix[c][d];
+			if(Matrix[c][d] < 10) // немного красивости из-за выравнивания вертикальной черты. Работает до 3х значных чисел
+			{
+				cout << Matrix[c][d] << "  | ";
+			}
+			else 
+			{
+				cout << Matrix[c][d] << " | ";
+			}
+		}
+		cout << endl;
+	}
+	
+	cout << endl << endl;
+	
 	return 0;
 	
+}
+
+
+*/
+
+// Задача 10
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	int x,y;
+	int i,z,t,c,d;
+	
+	srand(time(0));
+	
+	int a,b;
+		
+	cout << "Введите количество строк и столбцов в массиве" << endl;
+	cin >> a >> b;
+	
+	const int Row = a;
+	const int Col = b;
+	
+	
+	
+	cout << "Введите максимум и минимум" << endl;
+	cin >> x >> y;
+	const int upperlim = x;
+	const int lowerlim = y;
+	
+	t = 0;
+	z = 0;
+	
+	int Matrix[Row][Col] = {};
+	
+	for (c = 0; c < Row; c++)
+	{
+		for(d = 0; d < Col; d++)
+		{
+			Matrix[c][d] = rand() % (x - y + 1);
+			if(Matrix[c][d] < 10) // немного красивости из-за выравнивания вертикальной черты. Работает до 3х значных чисел
+			{
+				cout << Matrix[c][d] << "  | ";
+			}
+			else 
+			{
+				cout << Matrix[c][d] << " | ";
+			}
+		}
+		cout << endl;
+	}
+	
+	cout << endl << endl;
+	
+	
+	for(c = 0; c < Row; c++)
+	{
+		for(d = 0; d < Col; d++)
+		{
+			z += Matrix[c][d]; 
+		}
+		cout << endl;
+		if(t < z)
+		{
+			t = z;
+			i = c + 1;
+		}
+		z = 0;
+	}
+	cout << "В строке под номером " << i << " находится наибольшая сумма, равная " << t << endl;
+	
+	return 0;
 }
